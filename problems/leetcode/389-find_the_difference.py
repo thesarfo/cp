d5bc3871068a7ef5c09b8dc6bdec40fdbc6e8625
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-02-20 09:22:45 +0000

    feat: add solution for leetcode 389

---
+++ problems/leetcode/389-find_the_difference.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for i in t:
+            if i not in s:
+                return i
+        return s
