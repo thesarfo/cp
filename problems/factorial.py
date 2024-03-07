50136796ae8017cb82ff4619395786ea8a71c2c1
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-07 07:32:58 +0000

    change factorial sol'n and add binary search

--- problems/factorial.py
+++ problems/factorial.py
@@ -2,8 +2,4 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-        f = n * factorial(n - 1)
-        print(f)  # print intermediate results (optional)
-        return f
-
-print(factorial(1))
+        return n * factorial(n - 1)
