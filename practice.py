017bfa357f083413d9667e1bf70dd41c9d4e4fc3
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-12 10:11:41 +0000

    prob 7

--- practice.py
+++ practice.py
@@ -2,7 +2,7 @@ t = int(input())
 
 for i in range(t):
     A, C = map(int, input().split())
-    B = (A + C) / 2
+    B = (A + C) // 2
     if B == int(B):
         print(int(B))
     else:
--- problems/codechef/7-make_avg.py
+++ problems/codechef/7-make_avg.py
@@ -7,3 +7,15 @@ for i in range(t):
         print(int(B))
     else:
         print(-1)
+
+# alt solution
+t = int(input())            
+for i in range(t):          
+    A, C = map(int, input().split())
+    B = (A + C) // 2
+    if A % 2 == 0 and C % 2 == 0:
+        print(B)
+    elif A % 2 != 0 and C % 2 != 0:
+        print(B)
+    else:
+        print(-1)
