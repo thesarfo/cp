74165125bf1b8b3164470a5fbbb84ac124440011
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-12 17:37:38 +0000

    leetcode 66

---
+++ problems/leetcode/66-plus_one.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            digits[i] += carry
+            carry = digits[i] // 10
+            digits[i] %= 10 
+        
+            if carry == 0:
+                break
+        
+        if carry:
+            digits.insert(0, carry)
+        
+        return digits
