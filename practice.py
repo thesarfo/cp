ded889d7382f4499758fda9fc98a3cf752fec4ba
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-03-13 11:29:06 +0000

    made some changes

--- practice.py
+++ practice.py
@@ -1,9 +1,10 @@
-t = int(input())
+def solution(numbers):
+    fin = []
+    for i in range(1,len(numbers)-1):
+        if (numbers[i-1] < numbers[i] > numbers[i+1]) or (numbers[i-1] > numbers[i] < numbers[i+1]):
+            fin.append(1)
+        else:
+            fin.append(0)
+    return fin
 
-for i in range(t):
-    A, C = map(int, input().split())
-    B = (A + C) // 2
-    if B == int(B):
-        print(int(B))
-    else:
-        print(-1)
+print(solution([1, 2, 1, 3, 4]))
