c55ee91717ce3a0d3a10b5ef1c204276cd531f61
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-08 16:12:54 +0000

    codechef 12

---
+++ problems/codechef/12-input_mirror.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        int N = read.nextInt();
+        read.nextLine();
+        String S = read.nextLine();
+		System.out.println(N + " " + S);
+	}
+}
