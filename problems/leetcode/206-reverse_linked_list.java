e0f59ec850c9226f92bc1e2caf8580a709106943
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-25 15:44:03 +0000

    leetcode 206 optimal solution

--- problems/leetcode/206-reverse_linked_list.java
+++ problems/leetcode/206-reverse_linked_list.java
@@ -9,3 +9,20 @@ class Solution {
       return newHead;
     }
   }
+
+/* Optimal Solution */
+class OptimalSolution {
+  public ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    ListNode curr = head;
+    ListNode forw = null;
+
+    while (curr != null) {
+      forw = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = forw;
+    }
+    return prev;
+  }
+}
