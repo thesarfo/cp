39e8ae06a6549caa98c4b562f460c8c963c75e2a
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-07-16 12:26:11 +0000

    leetcode 125

--- problems/leetcode/344-reverse_string.java
+++ problems/leetcode/344-reverse_string.java
@@ -2,7 +2,6 @@ class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length - 1;
         while (left < right) {
-            // Swap the characters at indices left and right
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
