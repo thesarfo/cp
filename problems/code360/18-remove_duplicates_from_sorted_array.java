4e318ea32fa298aeb621a68af48996b245ae5b91
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-06 17:14:51 +0000

    code360 18

---
+++ problems/code360/18-remove_duplicates_from_sorted_array.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+    public static int removeDuplicates(int[] arr,int n) {
+        // Write your code here.
+        HashSet<Integer> result = new HashSet<>();
+
+        for(int i = 0; i < n; i++){
+            result.add(arr[i]);
+        }
+
+        int idx = 0;
+
+        for(int item: result){
+            arr[idx] = item;
+            idx++;
+        }
+        return result.size();
+    }
+}
