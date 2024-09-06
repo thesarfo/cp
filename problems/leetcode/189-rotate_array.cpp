db2131de501b63696f32c842a21f16a05167ca28
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-06 18:07:42 +0000

    leetcode 189

---
+++ problems/leetcode/189-rotate_array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+
+private:
+    void reverse(vector<int> &arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+};
