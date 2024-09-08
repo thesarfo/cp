2d25148e179a2167b121a1ddcfd61a876a827b20
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-08 23:50:09 +0000

    leetcode 1051

---
+++ problems/leetcode/1051-height_checker.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = new int[heights.length];
+
+        for(int i = 0; i < heights.length; i++){
+            expected[i] = heights[i];
+        }
+
+        Arrays.sort(expected);
+
+        int res = 0;
+        for(int i = 0; i < heights.length; i++){
+            if(heights[i] != expected[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+}
