5a91e385eebc2d60e74d6566776ca93a1e208f2f
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-09 15:46:52 +0000

    code360 21

---
+++ problems/code360/21-merge_2_sorted_arrays.cpp
@@ -0,0 +1,39 @@
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    // Write your code here
+    int n1 = a.size();
+    int n2 = b.size();
+
+    int i = 0, j = 0;
+
+    vector<int> unionArr;
+
+    while(i < n1 && j < n2){
+        if (a[i] <= b[j]){
+            if(unionArr.size() == 0 || unionArr.back() != a[i]){
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        } else{
+            if(unionArr.size() == 0 || unionArr.back() != b[j]){
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while (i < n1) {
+        if (unionArr.empty() || unionArr.back() != a[i]) {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
+
+    while (j < n2) {
+        if (unionArr.empty() || unionArr.back() != b[j]) {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+
+    return unionArr;
+}
