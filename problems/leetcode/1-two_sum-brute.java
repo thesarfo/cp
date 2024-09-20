28707f839f70bb4d551a072f954df3a0a6feb216
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-20 11:19:33 +0000

    leetcode 1

---
+++ problems/leetcode/1-two_sum-brute.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if (i == j) continue;
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {0, 0};
+    }
+}
