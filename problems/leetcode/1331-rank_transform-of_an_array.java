f3f415f1ae6e3015c2f10f16c5c99b2c11cc2b6c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-03 07:02:40 +0000

    leetcode 1331

---
+++ problems/leetcode/1331-rank_transform-of_an_array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        
+
+        HashMap<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 1;
+        for (int num : sortedArr) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rankMap.get(arr[i]);
+        }
+        
+        return arr;
+    }
+}        
