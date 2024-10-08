0ee0285c83ee6e1d39674665bbb8884013a15568
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-08 08:29:39 +0000

    leetcode 1732 temp

---
+++ solutions/leetcode/1732.find-the-highest-altitude.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1732 lang=java
+ *
+ * [1732] Find the Highest Altitude
+ */
+
+// @lc code=start
+class Solution {
+    public int largestAltitude(int[] gain) {
+        
+    }
+}
+// @lc code=end
+
