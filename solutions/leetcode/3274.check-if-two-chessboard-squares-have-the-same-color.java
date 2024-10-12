159afe7fa9bb831771238001b8c72de6ac639c7e
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-12 17:54:20 +0000

    restructure

---
+++ solutions/leetcode/3274.check-if-two-chessboard-squares-have-the-same-color.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3274 lang=java
+ *
+ * [3274] Check if Two Chessboard Squares Have the Same Color
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+        
+    }
+}
+// @lc code=end
+
