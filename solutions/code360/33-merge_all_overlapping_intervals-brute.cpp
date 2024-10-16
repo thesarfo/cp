5257feae8fc3c731675cf3e630a33dcbecba5f9d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-16 14:29:52 +0000

    code360 33

---
+++ solutions/code360/33-merge_all_overlapping_intervals-brute.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	// Write your code here.
+	int n = arr.size();
+	sort(arr.begin(), arr.end());
+	vector<vector<int>> ans;
+
+	for(int i = 0; i < n; i++){
+		int start = arr[i][0];
+		int end = arr[i][1];
+
+		if(!ans.empty() && end <= ans.back()[1]){
+			continue;
+		}
+		for(int j = i + 1; j < n; j++){
+			if(arr[j][0] <= end){
+				end = max(end, arr[j][1]);
+			} else{
+				break;
+			}
+		}
+		ans.push_back({start, end});
+	}
+
+	return ans;
+	
+}
