fe79cb8e9d5860c92abbe48eeef681d97d9d839c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-25 14:21:58 +0000

    code360 35

---
+++ solutions/code360/25-subarray_with_maximum_product.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public static int subarrayWithMaxProduct(int []arr){
+        // Write your code here.
+        int n = arr.length; 
+
+        int pre = 1, suff = 1;
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0) pre = 1;
+            if (suff == 0) suff = 1;
+            pre *= arr[i];
+            suff *= arr[n - i - 1]; 
+            ans = Math.max(ans, Math.max(pre, suff));
+        }
+        return ans;
+    }
+}
