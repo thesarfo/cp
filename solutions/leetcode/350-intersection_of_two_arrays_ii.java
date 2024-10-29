fb3f1aeb54b2b742f5debc5116d17adeac4bca80
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-29 19:02:10 +0000

    lc 350

---
+++ solutions/leetcode/350-intersection_of_two_arrays_ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] resultArr = new int[result.size()];
+        for (int k = 0; k < result.size(); k++) {
+            resultArr[k] = result.get(k);
+        }
+        return resultArr;
+    }
+}
