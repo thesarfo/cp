0fe4f9f43ed98f0515863727674b94a278d66125
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-11-02 15:43:04 +0000

    simple java template

---
+++ solutions/codeforces/TemplateSimple.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class TemplateSimple {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader x = new FastReader();
+            FastWriter out = new FastWriter();
+            
+            // code here
+
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            if (System.getProperty("ONLINE_JUDGE") == null) {
+                try {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static class FastWriter {
+        BufferedWriter bwr;
+
+        public FastWriter() {
+            bwr = new BufferedWriter(new OutputStreamWriter(System.out));
+            if (System.getProperty("ONLINE_JUDGE") == null) {
+                try {
+                    bwr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        public void print(Object object) throws IOException {
+            bwr.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bwr.append("\n");
+        }
+
+        public void close() throws IOException {
+            bwr.close();
+        }
+    }
+}
