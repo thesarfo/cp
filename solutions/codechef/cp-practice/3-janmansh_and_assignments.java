9d04137f4fa633f9034ecbb7f40d189b058e1ed2
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-11-05 14:16:00 +0000

    cp practice 3 and 4

---
+++ solutions/codechef/cp-practice/3-janmansh_and_assignments.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int x = sc.nextInt();
+		    if((x + 3) <= 10){
+		        System.out.println("Yes");
+		    } else{
+		        System.out.println("No");
+		    }
+		}
+
+	}
+}
