ff9a139eb96bd4bb46c026d4e5a4fa8100182bc7
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-11-06 13:05:13 +0000

    cf d

--- solutions/codeforces/practice/bitpp.java
+++ solutions/codeforces/practice/bitpp.java
@@ -4,14 +4,15 @@ public class bitpp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        sc.nextLine();
         int x = 0;
 
         while(t-- > 0){
             String a = sc.nextLine();
             if(a.contains("++")){
-                x--;
-            } else{
-                x++;
+                x+=1;
+            } else if (a.contains("--")){
+                x-=1;
             }
         }
         System.out.println(x);
