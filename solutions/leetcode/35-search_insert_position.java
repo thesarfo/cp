175fafac885e07f6f94287638b0a0c4dbb4e274c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-11-27 18:47:33 +0000

    leetcode 35

--- solutions/leetcode/35-search_insert_position.java
+++ solutions/leetcode/35-search_insert_position.java
@@ -4,13 +4,13 @@ class Solution {
         int right = nums.length - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
-                return mid; 
-            } else if (nums[mid] < target) {
-                left = mid + 1; 
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                right = mid - 1; 
+                left = mid + 1;
             }
         }
         return left;
