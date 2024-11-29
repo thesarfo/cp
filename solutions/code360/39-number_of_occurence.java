c4ff143d6182bc8c5e7ed26137a282f88c4f0820
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-11-29 12:04:39 +0000

    code360 39

---
+++ solutions/code360/39-number_of_occurence.java
@@ -0,0 +1,38 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        int n = arr.length;
+        
+        int first = binarySearch(arr, target, true);
+        
+        if (first == -1) {
+            return 0;
+        }
+        
+        int last = binarySearch(arr, target, false);
+        
+        return last - first + 1;
+    }
+
+    int binarySearch(int[] arr, int target, boolean findFirst) {
+        int low = 0, high = arr.length - 1, result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (arr[mid] == target) {
+                result = mid;
+                if (findFirst) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
