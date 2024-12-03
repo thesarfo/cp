fb58b41bdc73685beca11f41549b8d85cb9c8c90
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-03 08:38:10 +0000

    leetcode 153

---
+++ solutions/leetcode/153-find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
+        ans = 2 ** 31 - 1
+
+        while low <= high:
+            mid = (low + high) / 2
+
+            if nums[low] <= nums[mid]:
+                ans = min(ans, nums[low])
+                low = mid + 1
+            else:
+                ans = min(ans, nums[mid])
+                high = mid - 1
+        
+        return ans
+        
