98178b0b2fdeae44b1fc53ef5303e087841ed89e
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-24 09:48:04 +0000

    cf ladder 2

---
+++ solutions/codeforces/ladders/lt1300/BeautifulMatrix.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BeautifulMatrix {
+
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader x = new FastReader();
+            FastWriter out = new FastWriter();
+            
+            // code here
+            int oneRow = 0, oneCol = 0;
+
+            for(int i = 1; i <= 5; i++){
+                for(int j = 1; j <= 5; j++){
+                    int value = x.nextInt();
+                    if(value == 1){
+                        oneRow = i;
+                        oneCol = j;
+                    }
+                }
+            }
+            int moves = Math.abs(oneRow - 3) + Math.abs(oneCol - 3);
+            System.out.println(moves);
+
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            if (System.getProperty("ONLINE_JUDGE") == null) {
+                try {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+    public static class FastWriter {
+        BufferedWriter bwr;
+
+        public FastWriter() {
+            bwr = new BufferedWriter(new OutputStreamWriter(System.out));
+            if (System.getProperty("ONLINE_JUDGE") == null) {
+                try {
+                    bwr = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt")));
+                } catch (Exception e) {
+                }
+            }
+        }
+
+        public void print(Object object) throws IOException {
+            bwr.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bwr.append("\n");
+        }
+
+        public void close() throws IOException {
+            bwr.close();
+        }
+    }
+}
