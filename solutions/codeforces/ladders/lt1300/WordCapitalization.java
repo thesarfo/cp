77432e04f6ac7ff2c307823d78586c01d945812c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-25 10:47:01 +0000

    codeforces 281A

--- solutions/codeforces/ladders/lt1300/WordCapitalization.java
+++ solutions/codeforces/ladders/lt1300/WordCapitalization.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Solution {
+class Solution {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String w = s.next();
         
-        System.out.println(w.charAt(0).toUpperCase() + w.substring(1));
+        System.out.println(Character.toUpperCase(w.charAt(0)) + w.substring(1));
     }
 }
