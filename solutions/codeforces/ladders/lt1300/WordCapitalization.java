0510bfb93092040aba87d99d2176d5ab35f2b7c3
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-12-25 10:44:07 +0000

    codeforces 281A

---
+++ solutions/codeforces/ladders/lt1300/WordCapitalization.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String w = s.next();
+        
+        System.out.println(w.charAt(0).toUpperCase() + w.substring(1));
+    }
+}
