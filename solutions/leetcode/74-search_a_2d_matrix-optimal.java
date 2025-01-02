c87fddcd65dd2d221e5fd6172302baf203d6d2e5
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-01-02 09:54:16 +0000

    leetcode 74 optimal solution

--- solutions/leetcode/74-search_a_2d_matrix-optimal.java
+++ solutions/leetcode/74-search_a_2d_matrix-optimal.java
@@ -1,26 +1,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] <= target && target <= matrix[i][matrix[i].length - 1]) {
-                return binarySearch(matrix[i], target);
-            }
-        }
-        return false;
-    }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = 0, high = n * m - 1;
 
-    public boolean binarySearch(int[] arr, int target) {
-        int low = 0, high = arr.length - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int row = mid / m, col = mid % m;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2; 
-
-            if (arr[mid] == target) {
-                return true;
-            } else if (arr[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return false;
     }
