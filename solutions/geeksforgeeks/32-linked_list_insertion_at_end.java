5dcf86c77f97f4b4395e47fffe471ea21547c7c3
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-03-06 17:13:54 +0000

    g4g 32

---
+++ solutions/geeksforgeeks/32-linked_list_insertion_at_end.java
@@ -0,0 +1,31 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    // Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x) {
+        Node newNode = new Node(x);
+        
+        if (head == null) {
+            return newNode;
+        }
+    
+        Node temp = head;
+        
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        
+        temp.next = newNode;
+        
+        return head;
+    }
+}
