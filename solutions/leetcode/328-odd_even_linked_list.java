7a577c5749a18a14b694c86c66c814a455ddcc59
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-07-15 22:12:35 +0000

    lc 328 another brute sln

--- solutions/leetcode/328-odd_even_linked_list.java
+++ solutions/leetcode/328-odd_even_linked_list.java
@@ -28,3 +28,39 @@ class Solution {
         return oddHead.next;
     }
 }
+
+
+// another solution
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+
+        List<Integer> ll = new ArrayList<>();
+        ListNode temp = head;
+
+        while (temp != null && temp.next != null) {
+            ll.add(temp.val);
+            temp = temp.next.next;
+        }
+        if (temp != null) ll.add(temp.val); // for odd length lists, temp will be at the last node
+
+        temp = head.next;
+
+        while (temp != null && temp.next != null) {
+            ll.add(temp.val);
+            temp = temp.next.next;
+        }
+        if (temp != null) ll.add(temp.val); // for even length lists, temp will be at the last node
+
+        temp = head;
+        int idx = 0;
+
+        while (temp != null) {
+            temp.val = ll.get(idx);
+            idx++;
+            temp = temp.next;
+        }
+
+        return head;
+    }
+}
