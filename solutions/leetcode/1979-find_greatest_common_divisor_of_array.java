f1f299caec3b988781b982c7fc18322d9e028de9
Ernest Sarfo <ernestsarfo100@gmail.com>
2026-07-18 19:13:12 +0000

    lc 1979

--- solutions/leetcode/1979-find_greatest_common_divisor_of_array.java
+++ solutions/leetcode/1979-find_greatest_common_divisor_of_array.java
@@ -5,11 +5,10 @@ class Solution {
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
-        while (max != 0) {
-            int temp = max;
-            max = min % max;
-            min = temp;
-        }
-        return min;
+        return gcd(min, max);
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
