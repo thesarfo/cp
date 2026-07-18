d6894b25997812ff81f4e543827830a80a301aad
Ernest Sarfo <ernestsarfo100@gmail.com>
2026-07-18 19:12:32 +0000

    cf 1856A

---
+++ solutions/codeforces/practice/tales_of_a_sort.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A {
+    public static void main(String[] args) throws IOException {
+        try {
+            FastReader x = new FastReader();
+            FastWriter out = new FastWriter();
+
+            int t = x.nextInt();
+            StringBuilder sb = new StringBuilder();
+            while (t-- > 0) {
+                int n = x.nextInt();
+                long[] a = new long[n];
+                for (int i = 0; i < n; i++) a[i] = x.nextLong();
+
+                long ops = 0;
+                long cur = a[n - 1];
+                for (int i = n - 2; i >= 0; i--) {
+                    if (a[i] >= cur) {
+                        ops += a[i] - (cur - 1);
+                        cur = cur - 1;
+                    } else {
+                        cur = a[i];
+                    }
+                }
+                sb.append(ops).append("\n");
+            }
+            out.print(sb.toString());
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+
+    static class FastWriter {
+        BufferedWriter bw;
+        public FastWriter() {
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        void print(Object obj) { bw.write(obj.toString()); }
+        void println(Object obj) { bw.write(obj.toString() + "\n"); }
+        void close() { try { bw.close(); } catch (IOException e) {} }
+    }
+}
