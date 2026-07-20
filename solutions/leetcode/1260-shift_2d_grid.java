7f2a646e6a32e1ae7e53ebe833089b2879b4c23b
Ernest Sarfo <ernestsarfo100@gmail.com>
2026-07-20 20:53:47 +0000

    lc 1260

--- solutions/leetcode/1260-shift_2d_grid.java
+++ solutions/leetcode/1260-shift_2d_grid.java
@@ -2,9 +2,6 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        int total = m * n;
-        k = k % total;
-
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
@@ -13,6 +10,9 @@ class Solution {
             }
             result.add(row);
         }
+
+        int total = m * n;
+        k = k % total;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
