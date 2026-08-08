779e902ec121dea632bee775b43bbeed1adcc676
Ernest Sarfo <ernestsarfo100@gmail.com>
2026-08-08 08:31:21 +0000

    cf

---
+++ solutions/codeforces/practice/watermelon.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if(a % 2 == 0 && a > 2){
+        cout << "YES" << "\n";
+    } else {
+        cout << "NO" << "\n";
+    }
+
+   
+}
